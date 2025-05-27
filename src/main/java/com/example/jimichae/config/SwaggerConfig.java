8d68a01cf93f833599977a7af27bdd84f570eb2c
Chae <oumuamua121@gmail.com>
2025-05-27 23:57:34 +0900

    Swagger cors 해결을 위한 배포된 서버 url 등록

--- src/main/java/com/example/jimichae/config/SwaggerConfig.java
+++ src/main/java/com/example/jimichae/config/SwaggerConfig.java
@@ -1,11 +1,14 @@
 package com.example.jimichae.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +19,13 @@ public class SwaggerConfig {
 			.info(new Info()
 				.title("JINMICHAE API")
 				.description("JINMICHAE API 명세서")
-				.version("v1.0.0"));
+				.version("v1.0.0"))
+			.servers(List.of(
+			new Server()
+				.url("http://localhost:8080")
+				.description("Local Server"),
+			new Server()
+				.url("https://jinmichae.jayden-bin.cc")
+				.description("Remote Server")));
 	}
 }
